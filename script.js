be8bc839f1b87a2f3d9488b97f76f9bec8a93419
Idanise <daniseiheme@gmail.com>
2022-03-24 19:17:14 +0100

    smooth scroll

--- script.js
+++ script.js
@@ -31,13 +31,39 @@ document.addEventListener('keydown', function (e) {
 })
 
 const btnScrollTo = document.querySelector('.btn--scroll-to')
-const section1 = document.querySelector9('.section--1')
+const section1 = document.querySelector('#section--1')
 
-btn.btnScrollTo.addEventListener('click', function (e) {
+btnScrollTo.addEventListener('click', function (e) {
   const s1Coords = section1.getBoundingClientRect()
   console.log(s1Coords)
+
+  console.log(e.target.getBoundingClientRect())
+  console.log('Current scroll (X/Y):', window.pageXOffset, window.pageYOffset)
+
+  console.log(
+    'Height/Width',
+    document.documentElement.clientHeight,
+    document.documentElement.clientWidth
+  )
+
+  //Scrolling
+  // window.scroll(
+  //   s1Coords.left + window.pageYOffset,
+  //   s1Coords.top + window.pageYOffset
+  // )
+
+  // window.scrollTo({
+  //   left: s1Coords.left + window.pageYOffset,
+  //   top: s1Coords.top + window.pageYOffset,
+  //   behavior: 'smooth'
+  // })
+
+  section1.scrollIntoView({ behavior: 'smooth' })
 })
 
+window.scroll()
+
+/*
 /////////////////////////////////////////////////////
 ////LECTURES
 /////////////////////////////////////////////////////
@@ -111,3 +137,4 @@ console.log(link.getAttribute('href'))
 
 //Data attributes
 console.log(logo.dataset.versionNumber)
+*/
