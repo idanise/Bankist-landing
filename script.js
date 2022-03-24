200c184193d05ecc27764dc185c737bddf55b26f
Idanise <daniseiheme@gmail.com>
2022-03-25 00:45:20 +0100

    tab functionality

--- script.js
+++ script.js
@@ -7,6 +7,8 @@ const modal = document.querySelector('.modal')
 const overlay = document.querySelector('.overlay')
 const btnCloseModal = document.querySelector('.btn--close-modal')
 const btnsOpenModal = document.querySelectorAll('.btn--show-modal')
+const btnScrollTo = document.querySelector('.btn--scroll-to')
+const section1 = document.querySelector('#section--1')
 
 const openModal = function (e) {
   e.preventDefault()
@@ -30,9 +32,7 @@ document.addEventListener('keydown', function (e) {
   }
 })
 
-const btnScrollTo = document.querySelector('.btn--scroll-to')
-const section1 = document.querySelector('#section--1')
-
+//Button Scrolling
 btnScrollTo.addEventListener('click', function (e) {
   const s1Coords = section1.getBoundingClientRect()
   console.log(s1Coords)
@@ -62,6 +62,54 @@ btnScrollTo.addEventListener('click', function (e) {
 })
 
 window.scroll()
+
+//Page Navigation - smooth scrolling
+//Without event delegation
+// document.querySelectorAll('.nav__link').forEach(function (el) {
+//   el.addEventListener('click', function (e) {
+//     e.preventDefault()
+//     const id = this.getAttribute('href')
+//     console.log(id)
+//     document.querySelector(id).scrollIntoView({ behavior: 'smooth' })
+//   })
+// })
+
+//Using event delegation to solve the above
+//1. Add event listener to common parent element
+//2. Determine what element originated the event
+
+document.querySelector('.nav__links').addEventListener('click', function (e) {
+  e.preventDefault()
+
+  //Matching strategy
+  if (e.target.classList.contains('nav__link')) {
+    const id = e.target.getAttribute('href')
+    console.log(id)
+    document.querySelector(id).scrollIntoView({ behavior: 'smooth' })
+  }
+})
+
+//Tabbed component
+const tabs = document.querySelectorAll('.operations__tab')
+const tabsContainer = document.querySelector('.operations__tab-container')
+const tabsContent = document.querySelectorAll('.operations__content')
+
+tabsContainer.addEventListener('click', function (e) {
+  const clicked = e.target.closest('.operations__tab')
+
+  //Guard clause
+  if (!clicked) return
+
+  //Active tab
+  tabs.forEach(t => t.classList.remove('operations__tab--active'))
+  clicked.classList.add('operations__tab--active')
+
+  //Activate content area
+  tabsContent.forEach(c => c.classList.remove('operations__content--active'))
+  document
+    .querySelector(`.operations__content--${clicked.dataset.tab}`)
+    .classList.add('operations__content--active')
+})
 
 /*
 /////////////////////////////////////////////////////
@@ -137,4 +185,83 @@ console.log(link.getAttribute('href'))
 
 //Data attributes
 console.log(logo.dataset.versionNumber)
+
+//EVENT LISTENERS
+const h1 = document.querySelector('h1')
+
+// h1.addEventListener('mouseenter', function () {
+//   alert('addEventListener: Great! You are reading a header')
+// })
+
+const alertH1 = function (e) {
+  alert('addEventListener: Great! You are reading a header')
+}
+
+h1.addEventListener('mouseenter', alertH1)
+
+setTimeout(() => h1.removeEventListener('mouseenter', alertH1), 3000)
+
+h1.addEventListener('copy', function (e) {
+  e.preventDefault()
+  console.log('You cant copy')
+})
+
+//Old school way of listening to events
+// h1.onclick = function () {
+//   alert('addEventListener: Great! You are reading a header')
+// }
+
+//Event propagation
+const randomInt = (max, min) =>
+  Math.floor(Math.random() * (max - min + 1) + min)
+const randomColor = () =>
+  `rgb(${randomInt(0, 255)}, ${randomInt(0, 255)}, ${randomInt(0, 255)})`
+console.log(randomColor(0, 255))
+
+document.querySelector('.nav__link').addEventListener(
+  'click',
+  function (e) {
+    this.style.backgroundColor = randomColor()
+    console.log('LINK', e.target, e.currentTarget)
+    console.log(e.currentTarget === this)
+
+    //Stop propagation
+    // e.stopPropagation()
+  },
+  false
+)
+
+document.querySelector('.nav__links').addEventListener('click', function (e) {
+  this.style.backgroundColor = randomColor()
+  console.log('CONTAINER', e.target, e.currentTarget)
+})
+
+document.querySelector('.nav').addEventListener('click', function (e) {
+  this.style.backgroundColor = randomColor()
+  console.log('NAV', e.target, e.currentTarget)
+})
+
+//DOM TRAVERSING
+//Going download: child
+console.log(h1.querySelectorAll('.highlight'))
+console.log(h1.childNodes)
+console.log(h1.children)
+h1.firstElementChild.style.color = 'white'
+h1.lastElementChild.style.color = 'darkblue'
+
+//Going upwards: parents
+console.log(h1.parentNode)
+console.log(h1.parentElement)
+
+h1.closest('.header').style.background = 'yellow'
+h1.closest('h1').style.background = 'purple'
+
+//Going sideways: siblings
+console.log(h1.previousElementSibling)
+console.log(h1.nextElementSibling)
+
+console.log(h1.previousSibling)
+console.log(h1.nextSibling)
+
+console.log(h1.parentElement.children)
 */
